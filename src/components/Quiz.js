0236c75eecced34f2b4cbb706b8a795b6d51db34
Cricky <crickmonster@gmail.com>
2021-03-29 22:54:08 -0400

    create quiz.js file

---
+++ src/components/Quiz.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { QuizData } from "./QuizData";
+
+export class Quiz extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Quiz;
