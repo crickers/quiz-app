7857245a96d5c2a133e0452fdfc8c72c97b6f8f1
Cricky <crickmonster@gmail.com>
2021-03-30 19:01:58 -0400

    setState

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,6 +3,50 @@ import { QuizData } from "./QuizData";
 import "./styles.css";
 
 export class Quiz extends Component {
+  //prevent user from clicking button until an answer is selected
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userAnswer: null,
+      currentIndex: 0,
+      options: [],
+      quizEnd: false,
+      score: 0,
+      disabled: true,
+    };
+  }
+  //return a single question from the quiz data
+
+  loadQuiz = () => {
+    const { currentIndex } = this.state;
+    this.setState(() => {
+      return {
+        question: QuizData[currentIndex].question,
+        options: QuizData[currentIndex].options,
+        answer: QuizData[currentIndex].answer,
+      };
+    });
+  };
+
+  //this function handles the click event for the next button
+
+  nextQuestionHandler = () => {
+    const { userAnswer, answer, score } = this.state;
+
+    if (userAnswer === answer) {
+      this.setstate({
+        score: score + 1,
+      });
+    }
+
+    this.setState({
+      currentIndex: this.state.currentIndex + 1,
+      userAnswer: null,
+    });
+  };
+
   render() {
     return <div></div>;
   }
