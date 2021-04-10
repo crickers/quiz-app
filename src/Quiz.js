8dfedbb35ebf114247bea0eb7efafc53b6ca4172
Cricky <crickmonster@gmail.com>
2021-04-09 21:39:45 -0400

    deleting unnecessary files

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,6 +2,25 @@ import React, { Component } from "react";
 import { QuizData } from "./QuizData";
 
 export class Quiz extends Component {
+
+    constructor(props){
+      super(props)
+
+      this.state={
+          userAnswer: null,
+          currentIndex: 0
+          options:[],
+          quizEnd: false,
+          score: 0,
+          disabled: true,
+      }
+    }
+
+
+
+
+
+
   render() {
     return <div></div>;
   }
