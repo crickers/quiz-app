d96bdcdae6cd2acbb971ccb034668908bf2c4d94
Cricky <crickmonster@gmail.com>
2021-04-09 21:52:14 -0400

    building quiz.js, numbers not changed?

--- src/Quiz.js
+++ src/Quiz.js
@@ -7,18 +7,50 @@ export class Quiz extends Component {
       super(props)
 
       this.state={
-          userAnswer: null,
-          currentIndex: 0
-          options:[],
-          quizEnd: false,
-          score: 0,
-          disabled: true,
+          userAnswer: null, //current users answer
+          currentIndex: 0 //current questions index
+          options: [], //the four options for answers
+          quizEnd: false, //determines if user is on the last question
+          score: 0,  //keeps score
+          disabled: true,  // determines status of buttons
       }
     }
 
+    loadQuiz = () => {
+      const {currentIndex} = this.state;
+      this.setState((){
+          return {
+            question: QuizData[currentIndex].question,
+            options: QuizData[currentIndex].options,
+            answer: QuizData[currentIndex].answer,
+          }
 
+      })
+    }
+//Click event handler for the Next button
+    nextQuestionHandler = () => {
+      const {userAnswer, answer, score} = this.state
 
+      if(userAnswer=== answer){
+        this.setState({
+          score: score + 1,
+        })
+      }
 
+      this.setState({
+          currentIndex: this.state.currentIndex + 1,
+          userAnswer: null
+      })
+
+      //Check if correct answer and increment score
+      if(userAnswer === answer){
+          this.setState({
+              score: score + 1
+          })
+      }
+  }
+
+  
 
 
   render() {
